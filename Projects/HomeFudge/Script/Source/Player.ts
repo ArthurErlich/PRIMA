f028518f515a6da8602a66455ef59190258f942f
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-04-26 18:45:15 +0200

    Update Player.ts

--- Projects/HomeFudge/Script/Source/Player.ts
+++ Projects/HomeFudge/Script/Source/Player.ts
@@ -2,6 +2,10 @@ namespace HomeFudge {
     import ƒ = FudgeCore;
     export class Player extends ƒ.Node {
 
+        public update(deltaSeconds:number){
+
+        }
+
         constructor() {
             super("Player");
 
