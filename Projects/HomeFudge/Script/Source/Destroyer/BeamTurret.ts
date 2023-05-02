c430c735b613a871bbd83d5bb91231c95f20c77c
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-05-02 20:23:39 +0200

    Renamed LaserTurret to BeamTurret

---
+++ Projects/HomeFudge/Script/Source/Destroyer/BeamTurret.ts
@@ -0,0 +1,14 @@
+namespace HomeFudge{
+    import ƒ = FudgeCore;
+    export class BeamTurret extends ƒ.Node{
+        
+        public fire(){
+            console.warn("NO FIRE HERE");
+        }
+
+        constructor(){
+            super("BamTurret");
+        }
+
+    }
+}
