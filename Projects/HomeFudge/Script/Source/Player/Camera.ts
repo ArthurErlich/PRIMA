a99a0bcf213b85def97644b8d1ae5d0d75fbf16e
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-05-03 15:40:12 +0200

    Fixed soudbug remove temp rotation

--- Projects/HomeFudge/Script/Source/Player/Camera.ts
+++ Projects/HomeFudge/Script/Source/Player/Camera.ts
@@ -3,33 +3,36 @@ namespace HomeFudge{
     export class Camera extends ƒ.Node{
         public attachedTo:ƒ.Node = null;
         public camComp:ƒ.ComponentCamera = null;
+        public camNode:ƒ.Node = null;
 
         private offset:ƒ.Vector3 = null;
 
         public attachToShip(ship:ƒ.Node):void{
             this.offset = JSONparser.toVector3(Config.camera.offset);
-            this.camComp.mtxPivot.translation = this.offset;
+            this.camNode.mtxLocal.translation = this.offset;
             
             this.attachedTo = ship;
             this.mtxLocal.set(ship.mtxWorld);
-            this.camComp.mtxPivot.rotation = new ƒ.Vector3(0,-270,0);//TODO: Sound Bug when Pivot is rotated 
+            this.camNode.mtxLocal.rotation = new ƒ.Vector3(0,-270,0);//TODO: Sound Bug when Pivot is rotated 
             //TODO: add node for campComp
 
             ship.addChild(this);
 
         }
         private update = (): void =>{
-            //TODO: remove test rotation
-            this.mtxLocal.rotateY(10*_deltaSeconds);
-
+             //TODO: remove test rotation
+            //  this.mtxLocal.rotateY(10*_deltaSeconds);
         }
         
         private init(){
             this.camComp = new ƒ.ComponentCamera();
-            this.camComp.projectCentral(1.77, 70, ƒ.FIELD_OF_VIEW.DIAGONAL, 0.1, 30000);
-            this.camComp = this.camComp;
+            this.camComp.projectCentral(1.77, 75, ƒ.FIELD_OF_VIEW.DIAGONAL, 0.1, 30000);
 
-            this.addComponent(this.camComp);
+            this.camNode = new ƒ.Node("camPivotNode");
+
+            this.camNode.addComponent(new ƒ.ComponentTransform());
+            this.camNode.addComponent(this.camComp);
+            this.addChild(this.camNode);
             this.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.ZERO())));
             
         }
